804bd0937b2725593a7d994d183c399d82c6db77
oistikbal <ofarukistikbal@hotmail.com>
2021-03-21 16:11:36 +0300

    Add simple move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,39 @@ public class PlayerController : Singleton<PlayerController>
         m_arrow = GameObject.Find("Arrow");
     }
 
+    void Update()
+    {
+        PlayerInput();
+    }
+
     void FixedUpdate()
     {
         RotateArrow();        
+    }
+
+    void PlayerInput() 
+    {
+        if (Input.touchCount > 0)
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot() 
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, m_arrow.transform.forward, out hit, 100f, 1 << LayerMask.NameToLayer("target")))
+            StartCoroutine(Move(hit));
+    }
+
+    IEnumerator Move(RaycastHit hit) 
+    {
+        while (transform.position != hit.point)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.fixedDeltaTime);
+            yield return null;
+        }
     }
 
     void RotateArrow() 
